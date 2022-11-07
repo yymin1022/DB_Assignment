eae9df638f1877071704b29138727c88ab8695c3
yymin1022 <yymin1022@gmail.com>
2022-11-08 03:32:43 +0900

    Update Main.java : Added JDBC Basic Code

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
+        Scanner sqlConnDataScanner = new Scanner(new File("SQL_CONN.txt"));
+        String[] sqlConnData = new String[3];
+        for(int i = 0; sqlConnDataScanner.hasNextLine(); i++){
+            sqlConnData[i] = sqlConnDataScanner.nextLine();
+        }
+
+        Connection connection = DriverManager.getConnection(sqlConnData[0], sqlConnData[1], sqlConnData[2]);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from USER_LIST");
+
+        resultSet.next();
+        String name = resultSet.getString("USER_ID");
+        System.out.println(name);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
