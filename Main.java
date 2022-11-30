a3eef4586a67a26197a6fd0af3fddf1ef0030700
yymin1022 <yymin1022@gmail.com>
2022-12-01 04:31:03 +0900

    Update Main : Added Program Menus Function

--- Main.java
+++ Main.java
@@ -51,9 +51,43 @@ public class Main{
         System.out.println("1. 사용자 정보 조회");
         System.out.println("2. 판매 상품 검색");
         System.out.printf("3. %s거래내역 조회\n", userType == 0 ? "전체 " : "");
-        
-        if(userType == 0){
+
+        if (userType == 0) {
             System.out.println("4. 상품 재고 관리");
         }
+
+        while(true){
+            System.out.print("\nPlease Input Menu : ");
+            Scanner menuScanner = new Scanner(System.in);
+            String inputMenu = menuScanner.nextLine();
+
+            if(inputMenu.equals("1")){
+                userInfoMenu();
+            }else if(inputMenu.equals("2")){
+                searchProductMenu();
+            }else if(inputMenu.equals("3")){
+                searchTransactionMenu();
+            }else if(userType == 0 && inputMenu.equals("4")){
+                registerProductMenu();
+            }else{
+                System.out.println("Wrong Input. Please Retry.");
+            }
+        }
+    }
+
+    static void searchProductMenu(){
+        System.out.println("Product Search Menu!");
+    }
+
+    static void searchTransactionMenu(){
+        System.out.println("Transaction History Search Menu!");
+    }
+
+    static void registerProductMenu(){
+        System.out.println("Product Register Menu for Admin!");
+    }
+
+    static void userInfoMenu(){
+        System.out.println("User Info Menu!");
     }
 }
