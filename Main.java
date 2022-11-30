2fbaf3a1e50eb0737986bbd03a96ecf1575c451a
yymin1022 <yymin1022@gmail.com>
2022-12-01 03:56:35 +0900

    Update Main : Added Login Logic

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.printf("%d", initLogin());
+    }
+
+    static int initLogin(){
+        String inputID, inputPW;
+        Scanner loginScanner = new Scanner(System.in);
+        inputID = loginScanner.nextLine();
+        inputPW = loginScanner.nextLine();
+
+        ArrayList<String> listUser = new ArrayList<>(Arrays.asList("yong", "sans"));
+        ArrayList<String> listPW = new ArrayList<>(Arrays.asList("1022", "1234"));
+        if(listUser.contains(inputID)){
+            int userIdx = listUser.indexOf(inputID);
+
+            if(inputPW.equals(listPW.get(userIdx))){
+                //Login Success
+                return 0;
+            }
+
+            // Wrong Password
+            return -2;
+        }
+
+        // No Such User
+        return -1;
     }
 }
