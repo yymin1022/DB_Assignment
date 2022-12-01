a29c850e24ed81271b9f68786839d97ebbd696b1
yymin1022 <yymin1022@gmail.com>
2022-12-02 05:08:35 +0900

    Update Main : Cleaned UP

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Database Design Report 4 : Dept. Software 20194094 유용민");
         while(true){
+            System.out.println("********************");
+            System.out.println("Please Select Menu.");
             System.out.println("1. Search Products");
             System.out.println("2. Search Transaction History");
             System.out.println("3. Search User Info");
@@ -14,18 +16,23 @@ public class Main{
             String userInput = menuScanner.nextLine();
             switch (userInput) {
                 case "1":
+                    System.out.println("********************");
                     searchProductMenu();
                     break;
                 case "2":
+                    System.out.println("********************");
                     searchTransactionMenu();
                     break;
                 case "3":
+                    System.out.println("********************");
                     searchUserMenu();
                     break;
                 case "4":
+                    System.out.println("********************");
                     System.out.println("Exit. Thank you for Using.");
                     return;
                 default:
+                    System.out.println("********************");
                     System.out.println("Wrong Input. Please Try Again.");
                     break;
             }
