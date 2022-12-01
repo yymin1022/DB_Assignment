a717af1b7149fe28cdfb5dbbfa7f3ad26ab7b331
yymin1022 <yymin1022@gmail.com>
2022-12-02 05:04:12 +0900

    Update Main : Added Basic Input Logic on Main Menu

--- Main.java
+++ Main.java
@@ -1,10 +1,29 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello, DB Assignment!");
+        System.out.println("Database Design Report 4 : Dept. Software 20194094 유용민");
+        System.out.println("1. Search Products");
+        System.out.println("2. Search Transaction History");
+        System.out.println("3. Search User Info");
+
+        Scanner menuScanner = new Scanner(System.in);
+        System.out.print("Please Select Menu : ");
+        String userInput = menuScanner.nextLine();
+        switch(userInput){
+            case "1":
+                System.out.println("1. Search Products");
+                break;
+            case "2":
+                System.out.println("2. Search Transaction History");
+                break;
+            case "3":
+                System.out.println("3. Search User Info");
+                break;
+            default:
+                System.out.println("Wrong Input. Please Try Again.");
+                break;
+        }
     }
 
     static void searchProductMenu(){
