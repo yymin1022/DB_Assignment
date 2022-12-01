d8dd5be66757f68a6978837b092a33a98e46b5c3
yymin1022 <yymin1022@gmail.com>
2022-12-02 05:05:40 +0900

    Update Main : Added Loop Logic on Main Menu

--- Main.java
+++ Main.java
@@ -2,27 +2,33 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Database Design Report 4 : Dept. Software 20194094 유용민");
-        System.out.println("1. Search Products");
-        System.out.println("2. Search Transaction History");
-        System.out.println("3. Search User Info");
+        while(true){
+            System.out.println("Database Design Report 4 : Dept. Software 20194094 유용민");
+            System.out.println("1. Search Products");
+            System.out.println("2. Search Transaction History");
+            System.out.println("3. Search User Info");
+            System.out.println("4. Exit Program");
 
-        Scanner menuScanner = new Scanner(System.in);
-        System.out.print("Please Select Menu : ");
-        String userInput = menuScanner.nextLine();
-        switch(userInput){
-            case "1":
-                System.out.println("1. Search Products");
-                break;
-            case "2":
-                System.out.println("2. Search Transaction History");
-                break;
-            case "3":
-                System.out.println("3. Search User Info");
-                break;
-            default:
-                System.out.println("Wrong Input. Please Try Again.");
-                break;
+            Scanner menuScanner = new Scanner(System.in);
+            System.out.print("Please Select Menu : ");
+            String userInput = menuScanner.nextLine();
+            switch (userInput) {
+                case "1":
+                    System.out.println("1. Search Products");
+                    break;
+                case "2":
+                    System.out.println("2. Search Transaction History");
+                    break;
+                case "3":
+                    System.out.println("3. Search User Info");
+                    break;
+                case "4":
+                    System.out.println("Exit. Thank you for Using.");
+                    return;
+                default:
+                    System.out.println("Wrong Input. Please Try Again.");
+                    break;
+            }
         }
     }
 
