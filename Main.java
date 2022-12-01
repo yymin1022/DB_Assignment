82767e7b970b7f9baa27ab4554e6f2f33566b10f
yymin1022 <yymin1022@gmail.com>
2022-12-02 05:24:13 +0900

    Update Main : Added Basic Input Logic on Product Menu

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main{
             Scanner menuScanner = new Scanner(System.in);
             System.out.print("Please Select Menu : ");
             String userInput = menuScanner.nextLine();
-            switch (userInput) {
+            switch(userInput){
                 case "1":
                     System.out.println("********************");
                     searchProductMenu();
@@ -40,7 +40,36 @@ public class Main{
     }
 
     static void searchProductMenu(){
-        System.out.println("Product Search Menu!");
+        while(true){
+            System.out.println("Please Select Product Search Menu.");
+            System.out.println("1. List All Products");
+            System.out.println("2. List All Available Products");
+            System.out.println("3. Search by Product Name");
+            System.out.println("4. Search by Product Type");
+            System.out.println("5. Return to Main");
+
+            Scanner menuScanner = new Scanner(System.in);
+            System.out.print("Please Select Menu : ");
+            String userInput = menuScanner.nextLine();
+            switch(userInput){
+                case "1":
+                    System.out.println("List All Products");
+                    break;
+                case "2":
+                    System.out.println("List All Available Products");
+                    break;
+                case "3":
+                    System.out.println("Search by Product Name");
+                    break;
+                case "4":
+                    System.out.println("Search by Product Type");
+                    break;
+                case "5":
+                    return;
+                default:
+                    System.out.println("Wrong Input. Please Try Again.");
+            }
+        }
     }
 
     static void searchTransactionMenu(){
